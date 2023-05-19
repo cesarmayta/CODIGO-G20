a27b95163f2503e7033321b94d33f09551eae7cc
César Mayta <cesarmayta@gmail.com>
2023-05-18 23:04:40 -0500

    dia2 reto

---
+++ SEMANA01/dia2/07-reto_crud_personas.py
@@ -0,0 +1,11 @@
+import os
+import time
+"""
+CREAR CRUD DE PERSONAS
+IGUAL AL PROGRAMA DE EMPRESAS
+"""
+persona = {
+    'dni':'40404040',
+    'nombre':'cesar mayta',
+    'email':'cesar@gmail.com'
+}
