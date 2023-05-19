f74b36b0ac9c56623cafae1fb23d4196f4d117a3
César Mayta <cesarmayta@gmail.com>
2023-05-18 22:25:30 -0500

    dia2 crud terminado

--- SEMANA01/dia2/05-crud-empresas.py
+++ SEMANA01/dia2/05-crud-empresas.py
@@ -12,7 +12,7 @@ empresa = {
     'razon_social':'1 ONE S.A.C.',
     'ciudad':'LIMA'
 }
-listaEmpresas = []
+lista_empresas = []
 opcion = 0
 ANCHO = 50
 while(opcion != 5):
@@ -28,9 +28,77 @@ while(opcion != 5):
           [5] SALIR DEL PROGRAMA
           """)
     print("="*ANCHO)
-    opcion = int(input("INGRESE UNA OPCIÓN DEL MENU :"))
+    opcion = int(input("INGRESE UNA OPCIÓN DEL MENU :"))    
     os.system("clear")
-    print("su opción es : " + str(opcion))
+    
+    if(opcion == 1):
+        print("[1] REGISTRO DE NUEVA EMPRESA")
+        ruc = input("RUC : ")
+        razon_social = input("RAZÓN SOCIAL : ")
+        ciudad = input("CIUDAD : ")
+        dic_nueva_empresa = {
+            'ruc':ruc,
+            'razon_social':razon_social,
+            'ciudad':ciudad
+        }
+        lista_empresas.append(dic_nueva_empresa)
+    elif(opcion==2):
+        print("[2] RELACIÓN DE EMPRESAS")
+        for dic_empresa in lista_empresas:
+            print('*'*ANCHO)
+            for clave,valor in dic_empresa.items():
+                print(clave + " : " + valor)
+            print('*'*ANCHO)
+        input("presione ENTER para continuar...")
+    elif(opcion==3):
+        valor_busqueda = input('INGRESE RUC:')
+        posicion_busqueda = -1
+        for posicion in range(len(lista_empresas)):
+            dic_empresa = lista_empresas[posicion]
+            for clave,valor in dic_empresa.items():
+                if(clave=="ruc" and valor== valor_busqueda):
+                    posicion_busqueda = posicion
+                    break
+                
+        if(posicion_busqueda == -1):
+            print(" NO SE ENCONTRO EMPRESA")
+        else:
+            dic_empresa_busqueda = lista_empresas[posicion_busqueda]
+            print("EMPRESA ENCONTRADA : " + dic_empresa_busqueda.get('razon_social'))
+            razon_social = input("RAZÓN SOCIAL : ")
+            ciudad = input("CIUDAD : ")
+            dic_empresa_actualizar = {
+                'ruc':dic_empresa_busqueda.get('ruc'),
+                'razon_social':razon_social,
+                'ciudad':ciudad
+            }
+            lista_empresas[posicion_busqueda] = dic_empresa_actualizar
+            print("EMPRESA ACTUALIZADA")
+            
+            
+            
+    elif(opcion==4):
+        print("[4] ELIMINAR UNA EMPRESA")
+        valor_busqueda = input('INGRESE RUC:')
+        posicion_busqueda = -1
+        for posicion in range(len(lista_empresas)):
+            dic_empresa = lista_empresas[posicion]
+            for clave,valor in dic_empresa.items():
+                if(clave=="ruc" and valor== valor_busqueda):
+                    posicion_busqueda = posicion
+                    break
+                
+        if(posicion_busqueda == -1):
+            print(" NO SE ENCONTRO EMPRESA")
+        else:
+            lista_empresas.pop(posicion_busqueda)
+            print(" EMPRESA ELIMINADA !!!")
+    elif(opcion==5):
+        print("[5] ESTA SALIENDO DEL PROGRAMA")
+    else:
+        print("OPIÓN NO VALIDA!!!")
+    
+    
     time.sleep(1)
     os.system("clear")
     time.sleep(1)
