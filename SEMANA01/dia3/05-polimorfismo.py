9acae71b3c12a060398922bcdc6ddcdda732a911
César Mayta <cesarmayta@gmail.com>
2023-05-22 20:37:30 -0500

    dia3 herencia y polimorfismo

--- SEMANA01/dia3/05-polimorfismo.py
+++ SEMANA01/dia3/05-polimorfismo.py
@@ -18,5 +18,20 @@ class Alumno(Persona):
         super().mostrar()
         print('NOTA : ' + str(self.nota))
         
+class Profesor(Persona):
+    
+    def __init__(self,nom,ema,esp):
+        super().__init__(nom,ema)
+        self.especialidad = esp
+        
+    def mostrar(self):
+        print('*'*20 + "DATOS DEL PROFESOR")
+        super().mostrar()
+        print('ESPECIALIDAD : ' + self.especialidad)    
+        
 alumno1 = Alumno('carla perez','cperez@gmail.com',20)
 alumno1.mostrar()
+profesor1 = Profesor('Cesar Mayta','cesarmayta@gmail.com','BACKEND')
+profesor1.mostrar()
+profesor2 = Profesor('Jorge Garnica','jorge@gmail.com','FRONTEND')
+profesor2.mostrar()
