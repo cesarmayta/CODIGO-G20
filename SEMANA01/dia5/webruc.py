dda8ed536181c62c6afbc0e13906449e14169d3c
César Mayta <cesarmayta@gmail.com>
2023-05-23 22:59:44 -0500

    dia5 web ruc

--- SEMANA01/dia5/webruc.py
+++ SEMANA01/dia5/webruc.py
@@ -1,9 +1,33 @@
 from flask import Flask,request,render_template
+import requests
+import json
 
 app = Flask(__name__)
+api_token='ponaquitutoken'
+
 
 @app.route('/')
 def index():
     return render_template('ruc.html')
+
+@app.route('/consultaruc',methods=['POST'])
+def consulta_ruc():
+    razon_social = ''
+    if request.method == "POST":
+        ruc = request.form['ruc']
+        url = "https://apiperu.dev/api/ruc/"+ruc+"?api_token="+api_token
+        print(url)
+        response = requests.get(url)
+    
+        if response.status_code == 200:
+            data_ruc = json.loads(response.text)
+            print(data_ruc)
+            razon_social = data_ruc['data']['nombre_o_razon_social']
+            
+            
+        
+    return render_template('ruc.html',rsocial=razon_social)
+            
+        
 
 app.run(debug=True)
