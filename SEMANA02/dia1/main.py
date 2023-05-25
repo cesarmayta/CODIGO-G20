2e90e7277f22ee427a511b34cf191b23a681e78a
César Mayta <cesarmayta@gmail.com>
2023-05-24 20:03:58 -0500

    dia1 templates

--- SEMANA02/dia1/main.py
+++ SEMANA02/dia1/main.py
@@ -6,4 +6,16 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/portafolio')
+def portafolio():
+    return render_template('portafolio.html')
+
+@app.route('/acercade')
+def acercade():
+    return render_template('acercade.html')
+
+@app.route('/contacto')
+def contacto():
+    return render_template('contacto.html')
+
 app.run(debug=True)
