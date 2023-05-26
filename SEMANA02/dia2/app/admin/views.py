31d78f68f551a621244daa0d28971b356787b89e
César Mayta <cesarmayta@gmail.com>
2023-05-25 22:08:51 -0500

    dia2 auth

--- SEMANA02/dia2/app/admin/views.py
+++ SEMANA02/dia2/app/admin/views.py
@@ -1,6 +1,11 @@
 from flask import render_template,request
-
 from . import admin
+
+import pyrebase
+from app.firebase_config import firebaseConfig
+
+fb_app = pyrebase.initialize_app(firebaseConfig)
+auth = fb_app.auth()
 
 @admin.route('/')
 def index():
@@ -8,6 +13,16 @@ def index():
 
 
 """ VISTAS PARA LOGIN """
-@admin.route('/login')
+@admin.route('/login',methods=['GET','POST'])
 def login():
+    if request.method == "POST":
+        email = request.form['email']
+        password = request.form['password']
+        try:
+            usuario = auth.sign_in_with_email_and_password(email,password)
+            data_usuario = auth.get_account_info(usuario['idToken'])
+            print(data_usuario)
+        except:
+            print("usuario no valido")
+        
     return render_template('admin/login.html')
