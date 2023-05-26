3a7bc3f6d18096ed46b47170e9680326d825d0d8
César Mayta <cesarmayta@gmail.com>
2023-05-25 20:23:37 -0500

    dia2 blueprint admin

--- SEMANA02/dia2/app/__init__.py
+++ SEMANA02/dia2/app/__init__.py
@@ -2,10 +2,12 @@ from flask import Flask
 
 #importamos blueprints
 from .portafolio import portafolio
+from .admin import admin
 
 def create_app():
     app = Flask(__name__)
     
     app.register_blueprint(portafolio)
+    app.register_blueprint(admin)
     
     return app
