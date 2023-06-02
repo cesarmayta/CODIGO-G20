b6e3984e45127cd137deebc6ae563555c1203320
César Mayta <cesarmayta@gmail.com>
2023-06-01 22:18:15 -0500

    dia4 api rest listo

--- SEMANA03/dia4/frontend/src/App.jsx
+++ SEMANA03/dia4/frontend/src/App.jsx
@@ -1,13 +1,27 @@
 import React from 'react'
 import axios from 'axios'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import {Container,Table} from 'react-bootstrap'
+import {Container,Table,Form,Button} from 'react-bootstrap'
 
 class App extends React.Component{
   constructor(props){
     super(props);
     this.state = ({
-      tareas:[]
+      tareas:[],
+      descripcion:'',
+      estado:'pendiente',
+      id:0,
+      pos:null,
+      tituloBoton:'Agregar Tarea'
+    })
+    this.cambioDescripcion = this.cambioDescripcion.bind(this)
+    this.guardar = this.guardar.bind(this)
+  }
+
+  cambioDescripcion(e){
+    console.log(e.target.value)
+    this.setState({
+      descripcion : e.target.value
     })
   }
 
@@ -22,12 +36,48 @@ class App extends React.Component{
     })
   }
 
+  guardar(e){
+    e.preventDefault()
+    const dataTarea = {
+      descripcion : this.state.descripcion,
+      estado : this.state.estado
+    }
+
+    axios.post('http://localhost:5000/tarea',dataTarea)
+    .then(res=>{
+        console.log(res.data.content)
+        this.state.tareas.push(res.data.content)
+        var temp = this.state.tareas
+        this.setState({
+          descripcion:'',
+          tareas:temp,
+          tituloBoton:'Agregar Tarea',
+          pos:null,
+          id:0
+        }).catch((error)=>{
+          alert(error.toString())
+        })
+      }
+    )
+  }
+
   render(){
     return(
       <div>
         <Container>
           <h1>Lista de Tareas</h1>
-          <Table>
+          <Form onSubmit={this.guardar}>
+            <Form.Group className="mb-3">
+              <Form.Control type="text"
+                value={this.state.descripcion}
+                onChange={this.cambioDescripcion}
+              />
+              <Button variant="primary" type="submit">
+                {this.state.tituloBoton}
+              </Button>
+            </Form.Group>
+          </Form>
+          <Table striped bordered hover variant="dark">
             <thead>
               <tr>
                 <th>Id</th>
