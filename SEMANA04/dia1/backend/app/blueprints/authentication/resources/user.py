d57704ed7e50aa3e964b0f208ea41fa7b56d6861
Paolo Soncco <paholo2054@gmail.com>
2023-06-07 23:00:50 -0500

    jwt

--- SEMANA04/dia1/backend/app/blueprints/authentication/resources/user.py
+++ SEMANA04/dia1/backend/app/blueprints/authentication/resources/user.py
@@ -2,8 +2,9 @@ from flask_restful import Resource, Api
 from flask import request
 from .. import authentication
 from ..models import User
+from utils.db import db
 
-from werkzeug.security import generate_password_hash
+from werkzeug.security import generate_password_hash, check_password_hash
 from flask_jwt_extended import create_access_token, create_refresh_token
 
 api = Api(authentication)
@@ -33,10 +34,41 @@ class UserResource(Resource):
                 'message': 'El email ya esta registrado'
             }, 400
         except Exception as e:
+            db.session.rollback()
             return {
                 'message': str(e),
                 'status': False
             }, 500
         
+class SignInResource(Resource):
+    def post(self):
+        try:
+            json = request.get_json()
+            record = User.query.filter_by(email=json['email']).first()
+            if not record:
+                return {
+                    'status': False,
+                    'message': 'Usuario o contraseña incorrectos'
+                }, 400
+            is_pwd_secure = check_password_hash(record.password, json['password'])
+            if is_pwd_secure:
+              access_token = create_access_token(identity=record.id)
+              refresh_token = create_refresh_token(identity=record.id)
+              return {
+                  'status': True,
+                  'access_token': access_token,
+                  'refresh_token': refresh_token
+              }, 200
+            return {
+                'status': False,
+                'message': 'Usuario o contraseña incorrectos'
+            }, 400
+        except Exception as e:
+            return {
+                'status': False,
+                'message': str(e)
+            }, 500
+        
 
 api.add_resource(UserResource, '/user')
+api.add_resource(SignInResource, '/signin')
--- SEMANA04/dia1/backend/app/config.py
+++ SEMANA04/dia1/backend/app/config.py
@@ -1,4 +1,4 @@
 class Config:
     SQLALCHEMY_DATABASE_URI='mysql://root:root@localhost/db_shop_g20'
     SQLALCHEMY_TRACK_MODIFICATIONS=False
-    JWT_SECRET_KEY='secret'
+    JWT_SECRET_KEY='asdfhasdfhlkasdfl32h'
