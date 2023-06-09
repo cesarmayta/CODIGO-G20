484ff8e1285866e33bb9628eefac20a174b1ae12
Cesar codigo <cmaytadatabaseg1@gmail.com>
2023-06-08 19:48:25 -0500

    cors

--- SEMANA04/dia1/backend/app/__init__.py
+++ SEMANA04/dia1/backend/app/__init__.py
@@ -1,11 +1,14 @@
 from flask import Flask
 
+from flask_cors import CORS
 from .blueprints.shop import shop
 from .config import Config
 
 def create_app():
     
     app = Flask(__name__)
+    
+    CORS(app)
     
     app.config.from_object(Config)
     
