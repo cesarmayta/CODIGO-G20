f0576bdc98895b6cb7d5c596b3e5e4cc0bb8c8b4
Paolo Soncco <paholo2054@gmail.com>
2023-06-13 21:00:02 -0500

    flask testing

--- SEMANA04/flask_testing/app.py
+++ SEMANA04/flask_testing/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -6,19 +6,34 @@ app = Flask(__name__)
 def index():
     return 'Hello, World!'
 
-@app.route('/users')
+@app.route('/users', methods=['GET', 'POST'])
 def getAllUsers():
-    try:
-      response = [{
-          'name': "Eduardo",
-          'email': "eduardo@gmail.com"
-      }]
-      return response, 200
-    except Exception as e:
-        return {
-            'message': 'Internal server error',
-            'error': str(e)
-        }, 500
+    method = request.method
+    if method =='GET':
+      try:
+        response = [{
+            'name': "Eduardo",
+            'email': "eduardo@gmail.com"
+        }]
+        return response, 200
+      except Exception as e:
+          return {
+              'message': 'Internal server error',
+              'error': str(e)
+          }, 500
+    elif method == 'POST':
+       try:
+          json = request.get_json()
+          return {
+             'id': 1,
+             'name': json['name'],
+             'email': json['email']
+          }, 201
+       except Exception as e:
+          return {
+              'message': 'Internal server error',
+              'error': str(e)
+          }, 500
 
 if __name__ == '__main__':
     app.run(debug=True)
--- SEMANA04/flask_testing/test/test_app.py
+++ SEMANA04/flask_testing/test/test_app.py
@@ -14,9 +14,35 @@ class Test:
         assert response.data == b'Hello, World!'
 
     def test_users_get(self, client: Flask):
+        "Listar todos los usuarios"
         response = client.get('/users')
         assert response.status_code == 200
         assert response.json == [{
             'name': "Eduardo",
             'email': "eduardo@gmail.com"
         }]
+
+    def test_users_post(self, client: Flask):
+        """Crear un usuario"""
+        response = client.post('/users',json={
+            'name': 'Cesar Mayta',
+            'email': 'cesar@gmail.com'
+        })
+        assert response.status_code == 201
+        assert response.json == {
+            'id': 1,
+            'name': 'Cesar Mayta',
+            'email': 'cesar@gmail.com'
+        }
+
+    def test_users_post_error(self, client: Flask):
+        """Error al crear un usuario"""
+        response = client.post('/users', json={
+          'name': 'Cesar Mayta'
+        })
+        assert response.status_code == 500
+        assert 'message' in response.json
+        assert 'error' in response.json
+        assert response.json['message'] == 'Internal server error'
+        assert isinstance(response.json['error'], str)
+        assert response.json['error'] != ''
