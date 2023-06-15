8d01b15a3be07f44ee0edf8b6d515ca5bde1016a
Cesar codigo <cmaytadatabaseg1@gmail.com>
2023-06-14 22:49:33 -0500

    dia3 peliculas mtv

--- SEMANA05/dia3/peliculas/web/models.py
+++ SEMANA05/dia3/peliculas/web/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Pelicula(models.Model):
+    titulo = models.CharField(max_length=200)
+    imagen = models.CharField(max_length=200)
+    duracion = models.IntegerField()
+    
+    def __str__(self):
+        return self.titulo
--- SEMANA05/dia3/peliculas/web/views.py
+++ SEMANA05/dia3/peliculas/web/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 
+from .models import Pelicula
+
 # Create your views here.
 def index(request):
-    return render(request,'index.html')
+    lista_peliculas = Pelicula.objects.all() #select * from pelicula
+    print(lista_peliculas)
+    context = {
+        'peliculas':lista_peliculas
+    }
+    return render(request,'index.html',context)
