d921cefecc27e24150fcaeb6632cf3bd99ed7e4d
Cesar codigo <cmaytadatabaseg1@gmail.com>
2023-06-16 19:57:56 -0500

    posts

--- SEMANA05/dia5/web/views.py
+++ SEMANA05/dia5/web/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 
+from .models import Autor,Post
+
 # Create your views here.
 def index(request):
-    return render(request,'index.html')
+    list_post = Post.objects.all()
+    
+    context = {
+        'posts':list_post
+    }
+    return render(request,'index.html',context)
 
 def post(request):
     return render(request,'post.html')
