14a98def548da71e8737ab648fa6330b5d61cc64
Cesar codigo <cmaytadatabaseg1@gmail.com>
2023-06-19 21:49:40 -0500

    ckeditor

--- SEMANA06/dia1/ecommerce/web/admin.py
+++ SEMANA06/dia1/ecommerce/web/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.db import models
+from ckeditor.widgets import CKEditorWidget
 
 # Register your models here.
 from .models import (
@@ -8,5 +10,13 @@ from .models import (
 
 admin.site.register(Categoria)
 admin.site.register(Marca)
-admin.site.register(Producto)
 admin.site.register(ProductoImagen)
+
+@admin.register(Producto)
+class ProductoAdmin(admin.ModelAdmin):
+    list_display = ('nombre','categoria','marca','precio')
+    list_filter = ('categoria','marca')
+    list_editable = ('precio',)
+    formfield_overrides = {
+        models.TextField: {'widget':CKEditorWidget},
+    }
