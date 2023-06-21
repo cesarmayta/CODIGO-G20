23e32173fcb9f56c1079db294536c9d689881c64
Cesar codigo <cmaytadatabaseg1@gmail.com>
2023-06-20 20:30:35 -0500

    dia2 productos

--- SEMANA06/dia2/ecommerce/web/views.py
+++ SEMANA06/dia2/ecommerce/web/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
 
+
+from .models import (
+    Categoria,Marca,Producto
+)
 # Create your views here.
 def index(request):
-    return render(request,'index.html')
+    lista_productos = Producto.objects.all()
+    context = {
+        'productos':lista_productos
+    }
+    return render(request,'index.html',context)
