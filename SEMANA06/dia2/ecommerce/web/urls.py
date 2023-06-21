34949c9b40d5d8f6abf6d7cbb5169350ea82c454
Cesar codigo <cmaytadatabaseg1@gmail.com>
2023-06-20 21:47:03 -0500

    categorias

--- SEMANA06/dia2/ecommerce/web/urls.py
+++ SEMANA06/dia2/ecommerce/web/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name = 'web'
+
 urlpatterns = [
     path('',views.index,name='index'),
+    path('categoria/<int:categoria_id>',views.productos_por_categoria,name='categoria')
 ]
--- SEMANA06/dia2/ecommerce/web/views.py
+++ SEMANA06/dia2/ecommerce/web/views.py
@@ -7,7 +7,26 @@ from .models import (
 # Create your views here.
 def index(request):
     lista_productos = Producto.objects.all()
+    lista_categorias = Categoria.objects.all()
+    lista_marcas = Marca.objects.all()
+    
     context = {
-        'productos':lista_productos
+        'productos':lista_productos,
+        'categorias':lista_categorias,
+        'marcas':lista_marcas
+    }
+    return render(request,'index.html',context)
+
+def productos_por_categoria(request,categoria_id):
+    obj_categoria = Categoria.objects.get(pk=categoria_id)
+    #lista_productos = Producto.objects.filter(categoria=obj_categoria)
+    lista_productos = obj_categoria.producto_set.all()
+    lista_categorias = Categoria.objects.all()
+    lista_marcas = Marca.objects.all()
+    
+    context = {
+        'productos':lista_productos,
+        'categorias':lista_categorias,
+        'marcas':lista_marcas
     }
     return render(request,'index.html',context)
