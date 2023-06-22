44819fbe6fa48c73dae1b2eff1cec54a1ebb156e
Cesar codigo <cmaytadatabaseg1@gmail.com>
2023-06-21 19:31:58 -0500

    dia3

---
+++ SEMANA06/dia3/ecommerce/web/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+
+from . import views
+
+app_name = 'web'
+
+urlpatterns = [
+    path('',views.index,name='index'),
+    path('categoria/<int:categoria_id>',views.productos_por_categoria,name='categoria'),
+    path('marca/<int:marca_id>',views.productos_por_marca,name='marca'),
+    path('busqueda',views.producto_por_nombre,name='busqueda'),
+    path('producto/<int:producto_id>',views.producto_detalle,name='producto'),
+    path('carrito',views.carrito,name='carrito')
+]
