96bd1b503a6b0a3d6033cb081f151f71953da764
Cesar codigo <cmaytadatabaseg1@gmail.com>
2023-06-26 19:56:47 -0500

    cursos

--- SEMANA07/dia1/cursos/api/views.py
+++ SEMANA07/dia1/cursos/api/views.py
@@ -1,9 +1,20 @@
 from django.http import JsonResponse
+from .models import Curso
 
 def index(request):
     context = {
         'status':True,
         'content':'mi primer api rest con django'
+    }
+    
+    return JsonResponse(context)
+
+def curso(request):
+    lista_cursos = Curso.objects.all()
+    
+    context = {
+        'status':True,
+        'content':list(lista_cursos.values())
     }
     
     return JsonResponse(context)
