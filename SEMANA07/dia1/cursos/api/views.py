a2fa58dc614b39df0e10620c22079b15020d6af4
Cesar codigo <cmaytadatabaseg1@gmail.com>
2023-06-26 19:40:23 -0500

    dia1 index

--- SEMANA07/dia1/cursos/api/views.py
+++ SEMANA07/dia1/cursos/api/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.http import JsonResponse
 
-# Create your views here.
+def index(request):
+    context = {
+        'status':True,
+        'content':'mi primer api rest con django'
+    }
+    
+    return JsonResponse(context)
