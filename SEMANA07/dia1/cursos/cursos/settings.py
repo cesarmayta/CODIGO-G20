793ab9749e1cd640e29fd0339cac505a0d20ffe1
Cesar codigo <cmaytadatabaseg1@gmail.com>
2023-06-26 21:08:54 -0500

    dia1 drf

--- SEMANA07/dia1/cursos/cursos/settings.py
+++ SEMANA07/dia1/cursos/cursos/settings.py
@@ -32,6 +32,8 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'api',
+    'peliculas',
+    'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
