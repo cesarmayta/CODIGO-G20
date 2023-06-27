644d4c3f7111e14e0dab01a6970c6ddadad38ab7
Cesar codigo <cmaytadatabaseg1@gmail.com>
2023-06-26 22:58:58 -0500

    dia1 alumno

--- SEMANA07/dia1/api_con_drf/codigo/api/serializers.py
+++ SEMANA07/dia1/api_con_drf/codigo/api/serializers.py
@@ -2,9 +2,15 @@ from rest_framework import serializers
 
 from .models import Alumno
 
+"""
 class AlumnoSerializer(serializers.Serializer):
     nombre = serializers.CharField()
     email = serializers.EmailField()
     
     def create(self,validated_data):
         return Alumno.objects.create(**validated_data)
+"""
+class AlumnoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Alumno
+        fields = '__all__'
