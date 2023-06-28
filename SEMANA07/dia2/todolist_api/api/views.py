c8d760611cde6ec69816361974c804ac626d6c87
Cesar codigo <cmaytadatabaseg1@gmail.com>
2023-06-27 19:37:00 -0500

    dia2

---
+++ SEMANA07/dia2/todolist_api/api/views.py
@@ -0,0 +1,12 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+
+class IndexView(APIView):
+    
+    def get(self,request):
+        context = {
+            'status':True,
+            'content':'servidor activo'
+        }
+        return Response(context)
