8472c227c8e53797de8cf89570b49ee47b1d2038
Cesar codigo <cmaytadatabaseg1@gmail.com>
2023-06-27 20:39:47 -0500

    dia2 get put delete

--- SEMANA07/dia2/todolist_api/api/views.py
+++ SEMANA07/dia2/todolist_api/api/views.py
@@ -1,6 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from django.http import Http404
+from rest_framework import status
 
 from .models import Tarea
 from .serializers import TareaSerializer
@@ -39,6 +40,21 @@ class TareaDetailView(APIView):
         data = self.get_object(pk)
         serializer = TareaSerializer(data)
         return Response(serializer.data)
+    
+    def put(self,request,pk):
+        data = self.get_object(pk)
+        serializer = TareaSerializer(data,data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data)
+        
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self,request,pk):
+        data = self.get_object(pk)
+        data.delete()
+        
+        return Response(status=status.HTTP_204_NO_CONTENT)
     
     
     
