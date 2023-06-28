642e4c51cf3ca9578f3224f91d36f2fde7037288
Cesar codigo <cmaytadatabaseg1@gmail.com>
2023-06-27 20:03:41 -0500

    get y post

---
+++ SEMANA07/dia2/todolist_api/api/serializers.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+from .models import Tarea
+
+class TareaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tarea
+        fields = '__all__'
--- SEMANA07/dia2/todolist_api/api/views.py
+++ SEMANA07/dia2/todolist_api/api/views.py
@@ -1,6 +1,8 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
+from .models import Tarea
+from .serializers import TareaSerializer
 
 class IndexView(APIView):
     
@@ -10,3 +12,20 @@ class IndexView(APIView):
             'content':'servidor activo'
         }
         return Response(context)
+    
+class TareaView(APIView):
+    
+    def get(self,request):
+        data = Tarea.objects.all()
+        serializer = TareaSerializer(data,many=True)
+        return Response(serializer.data)
+    
+    def post(self,request):
+        serializer = TareaSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+    
+    
+    
+    
