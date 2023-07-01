88f1064370d1d94b8e5dabfff42eba3d9324b6e4
Cesar codigo <cmaytadatabaseg1@gmail.com>
2023-07-01 11:50:03 -0500

    swagger

--- SEMANA07/dia5/pos_backend/pos_backend/urls.py
+++ SEMANA07/dia5/pos_backend/pos_backend/urls.py
@@ -15,15 +15,37 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,TokenVerifyView
 )
+
+""" para swagger """
+from django.urls import re_path
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="POS API",
+      default_version='v1',
+      description="API Rest para sistema punto de venta",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="cmaytacodigog20@gmail.com"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+""" -------------- """
 
 urlpatterns = [
     path('',include('api.urls')),
     path('admin/', admin.site.urls),
     path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('verify/', TokenVerifyView.as_view(), name='token_verify'),
-    path('api/admin/',include('api_admin.urls'))
+    path('api/admin/',include('api_admin.urls')),
+    path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
