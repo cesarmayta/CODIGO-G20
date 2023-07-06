a7da8276f7d35a1d7e9a76549f0674f35c9dc075
Cesar codigo <cmaytadatabaseg1@gmail.com>
2023-07-05 21:03:50 -0500

    dia3 categorias get

--- SEMANA08/dia3/api-ofertas/src/index.js
+++ SEMANA08/dia3/api-ofertas/src/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const {config} = require('./config')
 
+const categoriaApi = require('./routes/categoria.routes')
+
 const app = express()
 
 app.get('/',(req,res)=>{
@@ -9,5 +11,7 @@ app.get('/',(req,res)=>{
         'content':'servidor activo'
     })
 })
+
+categoriaApi(app)
 
 app.listen(config.port,()=>console.log('http://localhost:'+config.port))
