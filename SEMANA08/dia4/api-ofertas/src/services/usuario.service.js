461417c93a7240a2d861fcb0e1d88ee5b9b29da2
Cesar codigo <cmaytadatabaseg1@gmail.com>
2023-07-06 22:17:21 -0500

    usuario service

---
+++ SEMANA08/dia4/api-ofertas/src/services/usuario.service.js
@@ -0,0 +1,33 @@
+const MysqlLib = require('../lib/mysql')
+const bcrypt = require('bcryptjs')
+
+class UsuarioService{
+
+    constructor(){
+        this.db = new MysqlLib()
+    }
+
+    async getLast(){
+        const sqlLast = `select usuario_id as id,
+                         usuario_nombre as usuario
+                         from tbl_usuario
+                         order by usuario_id desc limit 1`
+        const result = await this.db.querySql(sqlLast,[])
+        return result
+    }
+    async create({usuario}){
+        const passwordEncriptado = await bcrypt.hash(usuario.password,10)
+
+        const sqlCreate = `insert into tbl_usuario(usuario_nombre,usuario_password)
+                            values(?,?)
+                            `
+        await this.db.querySql(sqlCreate,[usuario.usuario,passwordEncriptado])
+        const result = await this.db.getLast()
+        return result
+
+    }
+
+    
+}
+
+module.exports = UsuarioService
