04889437d561486e38ad4fd1cea6cb0e1cc73247
Cesar codigo <cmaytadatabaseg1@gmail.com>
2023-07-06 22:57:24 -0500

    dia4 auth terminado

--- SEMANA08/dia4/api-ofertas/src/config/index.js
+++ SEMANA08/dia4/api-ofertas/src/config/index.js
@@ -5,7 +5,8 @@ const config = {
     mysql_user: process.env.MYSQL_USER || 'root',
     mysql_pwd: process.env.MYSQL_PWD || '',
     mysql_host: process.env.MYSQL_HOST || 'localhost',
-    mysql_db: process.env.MYSQL_DB || 'db_ofertas_g20'
+    mysql_db: process.env.MYSQL_DB || 'db_ofertas_g20',
+    jwt_secret:process.env.JWT_SECRET || 'qwerty123'
 }
 
 module.exports = {config}
--- SEMANA08/dia4/api-ofertas/src/routes/usuario.routes.js
+++ SEMANA08/dia4/api-ofertas/src/routes/usuario.routes.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const UsuarioService = require('../services/usuario.service')
+const jwt = require('jsonwebtoken')
+const {config} = require('../config')
 
 const boom = require('@hapi/boom')
 
@@ -19,6 +21,30 @@ function usuarioApi(app){
             })
         }catch(err){
             res.status(500).json(boom.badData(err))
+        }
+    })
+
+    router.post('/login',async function (req,res){
+        const {body: usuario} = req
+
+        const authUsuario = await objUsuario.authenticate({usuario})
+        if(authUsuario.id > 0){
+            const token = jwt.sign(
+                authUsuario,
+                config.jwt_secret,
+                {
+                    expiresIn:'1h'
+                }
+            )
+            res.status(200).json({
+                status:true,
+                content:token
+            })
+        }else{
+            res.status(401).json({
+                status:false,
+                content:'datos invalidos'
+            })
         }
     })
 }
