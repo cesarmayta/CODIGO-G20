abb7677dffef3c0105607ef19c29e4ce02e684c9
Cesar codigo <cmaytadatabaseg1@gmail.com>
2023-07-07 20:06:30 -0500

    token verify

--- SEMANA08/dia4/api-ofertas/src/middlewares/auth.handler.js
+++ SEMANA08/dia4/api-ofertas/src/middlewares/auth.handler.js
@@ -5,7 +5,20 @@ function verifyToken(req,res,next){
     const bearerToken = req.headers['authorization']
     console.log(bearerToken)
     if(typeof bearerToken !== 'undefined'){
-        return next()
+        //validamos el token
+        const bearer = bearerToken.split(' ')
+        const token = bearer[1]
+        console.log('token : ' + token)
+        try{
+            const decoded = jwt.verify(token,config.jwt_secret)
+            console.log(decoded)
+            return next()
+        }catch(err){
+            return res.status(401).json({
+                status:false,
+                content:err
+            })
+        }
     }
     else{
         res.status(403).json({
