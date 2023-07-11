0f6725481161e5187a292ec138b682863461ec66
Cesar codigo <cmaytadatabaseg1@gmail.com>
2023-07-10 20:16:46 -0500

    prisma get

--- SEMANA08/dia6/pos_backend_prisma/index.js
+++ SEMANA08/dia6/pos_backend_prisma/index.js
@@ -1,12 +1,23 @@
 const express = require('express')
 const app = express()
 
+const {PrismaClient} = require('@prisma/client')
+const prisma = new PrismaClient()
+
 app.use(express.json())
 
 app.get('/',(req,res)=>{
     res.json({
         status:true,
         content:'servidor con prisma activo'
+    })
+})
+
+app.get('/categoria',async(req,res)=>{
+    const data = await prisma.tbl_categoria.findMany()
+    res.json({
+        status:true,
+        content:data
     })
 })
 
---
+++ SEMANA08/dia6/pos_backend_prisma/readme.md
@@ -0,0 +1,12 @@
+#comandos para prisma.js:
+
+#1 - para iniciar prisma
+
+npx prisma init
+
+#2 - mapear base de datos
+
+npx prisma db pull
+
+#3 - generar prisma cliente
+npx prisma generate
