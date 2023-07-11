8dacce72be6baff92269063f9a551f5ebaeef7c3
Cesar codigo <cmaytadatabaseg1@gmail.com>
2023-07-10 20:39:36 -0500

    dia6 prisma put delete

--- SEMANA08/dia6/pos_backend_prisma/index.js
+++ SEMANA08/dia6/pos_backend_prisma/index.js
@@ -21,4 +21,48 @@ app.get('/categoria',async(req,res)=>{
     })
 })
 
+app.post('/categoria',async(req,res)=>{
+    const newData = await prisma.tbl_categoria.create({
+        data:req.body
+    })
+    res.json({
+        status:true,
+        content:newData
+    })
+})
+
+app.get("/categoria/:id",async (req,res)=>{
+    const data = await prisma.tbl_categoria.findUnique({
+        where: {
+            categoria_id: parseInt(req.params.id)
+        }
+    })
+    res.json({
+        status:true,
+        content:data
+    })
+})
+
+app.put("/categoria/:id",async (req,res)=>{
+    const data = await prisma.tbl_categoria.update({
+        where: {
+            categoria_id: parseInt(req.params.id)
+        },
+        data:req.body
+    })
+    res.json({
+        status:true,
+        content:data
+    })
+})
+
+app.delete("/categoria/:id",async (req,res)=>{
+    const data = await prisma.tbl_categoria.delete({
+        where: {
+            categoria_id: parseInt(req.params.id)
+        }
+    })
+    res.sendStatus(200)
+})
+
 app.listen(5000,()=>console.log("http://localhost:5000"))
