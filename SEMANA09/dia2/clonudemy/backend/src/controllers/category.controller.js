fd2ab6c63a7f3aa6f28ec59c76e233743fef1d55
Cesar codigo <cmaytadatabaseg1@gmail.com>
2023-07-11 21:53:51 -0500

    dia2 categories post getbyid

--- SEMANA09/dia2/clonudemy/backend/src/controllers/category.controller.js
+++ SEMANA09/dia2/clonudemy/backend/src/controllers/category.controller.js
@@ -10,4 +10,28 @@ categoryController.getAll = async (req,res)=>{
     })
 }
 
+categoryController.create = async (req,res)=>{
+    try{
+        const newCategory = new CategoryModel(req.body)
+        await newCategory.save()
+        res.status(201).json({
+            status:true,
+            content:newCategory
+        })
+    }catch(err){
+        res.status(502).json({
+            status:false,
+            content:err
+        })
+    }
+}
+
+categoryController.getOne = async (req,res)=>{
+    const category = await CategoryModel.findById(req.params.id)
+    res.json({
+        status:true,
+        content:category
+    })
+}
+
 module.exports = categoryController
--- SEMANA09/dia2/clonudemy/backend/src/routes/category.route.js
+++ SEMANA09/dia2/clonudemy/backend/src/routes/category.route.js
@@ -1,9 +1,14 @@
 const {Router} = require('express')
 const router = Router()
 
-const {getAll} = require('../controllers/category.controller')
+const {getAll,create,getOne} = require('../controllers/category.controller')
 
 router.route('/')
 .get(getAll)
+.post(create)
+
+router.route('/:id')
+.get(getOne)
+
 
 module.exports = router
