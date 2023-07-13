d291912168abe5f3efe5161915bbbae583cddbf8
Cesar codigo <cmaytadatabaseg1@gmail.com>
2023-07-12 21:51:48 -0500

    user auth

--- SEMANA09/dia3/clonudemy/backend/src/config/index.js
+++ SEMANA09/dia3/clonudemy/backend/src/config/index.js
@@ -2,7 +2,8 @@ require('dotenv').config()
 
 const config = {
     port: process.env.PORT || '5000',
-    mongoUri : process.env.MONGOURI || 'mongodb://127.0.0.1:27017/db_clon_udemy'
+    mongoUri : process.env.MONGOURI || 'mongodb://127.0.0.1:27017/db_clon_udemy',
+    jwt_secret: process.env.JWT_SECRET || 'qwerty123'
 }
 
 module.exports = {config}
