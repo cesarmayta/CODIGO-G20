a66d84da454fc9f161a3897789ac28255474d7d9
Cesar codigo <cmaytadatabaseg1@gmail.com>
2023-07-13 21:26:34 -0500

    dia4 middlewares

--- SEMANA09/dia4/clonudemy/backend/src/routes/category.route.js
+++ SEMANA09/dia4/clonudemy/backend/src/routes/category.route.js
@@ -1,16 +1,18 @@
 const {Router} = require('express')
 const router = Router()
 
+const {verifyToken} = require('../middlewares/auth.handler')
+
 const {getAll,create,getOne,updateOne,deleteOne} = require('../controllers/category.controller')
 
 router.route('/')
 .get(getAll)
-.post(create)
+.post(verifyToken,create)
 
 router.route('/:id')
-.get(getOne)
-.put(updateOne)
-.delete(deleteOne)
+.get(verifyToken,getOne)
+.put(verifyToken,updateOne)
+.delete(verifyToken,deleteOne)
 
 
 module.exports = router
