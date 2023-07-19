4b5fb876ea210190f9b9d7336fcd4e096a26881f
Cesar codigo <cmaytadatabaseg1@gmail.com>
2023-07-18 20:35:51 -0500

    dia2

--- SEMANA10/backend/src/libs/mongoose.lib.js
+++
@@ -1,8 +0,0 @@
-const {config} = require('../config')
-const mongoose = require('mongoose')
-
-mongoose.Promise = global.Promise
-
-mongoose.connect(config.mongoUri)
-
-console.log('conectado a mongodb')
