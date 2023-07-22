81ea07adaf9dab5d39b5e4126dfb41da680a908c
Cesar codigo <cmaytadatabaseg1@gmail.com>
2023-07-22 10:15:01 -0500

    dia6 socket io

--- SEMANA10/dia6/websockets/index.js
+++ SEMANA10/dia6/websockets/index.js
@@ -5,4 +5,13 @@ const port = 5000
 
 app.use(express.static('public'))
 
-app.listen(port,()=>console.log('http://localhost:'+port))
+const server = app.listen(port,()=>console.log('http://localhost:'+port))
+
+/************* web socket ***************/
+const SocketIO = require('socket.io')
+
+const io = SocketIO(server)
+
+io.on('connection',(socket)=>{
+    console.log('nueva conexión por web socket id : ',socket.id)
+})
